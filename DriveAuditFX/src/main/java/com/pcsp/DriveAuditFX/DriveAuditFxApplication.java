58ff1e80efc0642aa452b619a8df8815ae34c58d
malassila <90357470+malassila@users.noreply.github.com>
2023-02-14 22:32:20 -0500

    Initial Commit

---
+++ DriveAuditFX/src/main/java/com/pcsp/DriveAuditFX/DriveAuditFxApplication.java
@@ -0,0 +1,13 @@
+package com.pcsp.DriveAuditFX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriveAuditFxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DriveAuditFxApplication.class, args);
+	}
+
+}
---
+++ DriveAuditFX/src/test/java/com/pcsp/DriveAuditFX/DriveAuditFxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pcsp.DriveAuditFX;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriveAuditFxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
